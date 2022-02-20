3bc793185035601b8cac8d8fc32ed2ab18e08933
Eddy Rivera Echegaray <eddyrivera@Eddys-MacBook-Pro-2.local>
2022-02-20 13:32:02 +0100

    change BrowsetRouter to HashRouter

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "react-query";
 import App from "./App";
 
@@ -9,9 +9,9 @@ const queryClient = new QueryClient();
 ReactDOM.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </QueryClientProvider>
   </React.StrictMode>,
   document.getElementById("root")
